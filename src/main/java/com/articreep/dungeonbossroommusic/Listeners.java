6f327ee85e1230ecf1bf5baa592b41b1323277c4
Arti_Dev <boomcreeper296@gmail.com>
2021-04-26 10:32:28 -0400

    THEY FIXED THE COLON BUG YAY

--- src/main/java/com/articreep/dungeonbossroommusic/Listeners.java
+++ src/main/java/com/articreep/dungeonbossroommusic/Listeners.java
@@ -28,11 +28,11 @@ public class Listeners {
     		return;
     	}
     	// Most boss messages have a space on each side of the colon because Hypixel has them for some reason..?
-    	if (message.equals("[BOSS] Scarf : This is where the journey ends for you, Adventurers.")
+    	if (message.equals("[BOSS] Scarf: This is where the journey ends for you, Adventurers.")
     			|| message.equals("[BOSS] The Professor: I was burdened with terrible news recently...")
-    			|| message.equals("[BOSS] Thorn : Welcome Adventurers! I am Thorn, the Spirit! And host of the Vegan Trials!")
-    			|| message.equals("[BOSS] Livid : Welcome, you arrive right on time. I am Livid, the Master of Shadows.") 
-    			|| message.equals("[BOSS] Sadan : So you made it all the way here...and you wish to defy me? Sadan?!")
+    			|| message.equals("[BOSS] Thorn: Welcome Adventurers! I am Thorn, the Spirit! And host of the Vegan Trials!")
+    			|| message.equals("[BOSS] Livid: Welcome, you arrive right on time. I am Livid, the Master of Shadows.") 
+    			|| message.equals("[BOSS] Sadan: So you made it all the way here...and you wish to defy me? Sadan?!")
     			|| message.equals("[BOSS] Necron: Finally, I heard so much about you. The Eye likes you very much.")) {
     		stopmusic = false;
 	    	new Thread(new Runnable() {
@@ -47,11 +47,11 @@ public class Listeners {
 	    		
 	    	}).start();
 	    	
-    	} else if (message.equals("[BOSS] Scarf : His technique.. is too advanced..")
+    	} else if (message.equals("[BOSS] Scarf: His technique.. is too advanced..")
     			|| message.equals("[BOSS] Necron: Before I have to deal with you myself.")
     			|| message.contains("\u2620 Defeated Thorn in")
     			|| message.endsWith("My shadows are everywhere, THEY WILL FIND YOU!!")
-    			|| message.equals("[BOSS] Sadan : NOOOOOOOOO!!! THIS IS IMPOSSIBLE!!")
+    			|| message.equals("[BOSS] Sadan: NOOOOOOOOO!!! THIS IS IMPOSSIBLE!!")
     			|| message.equals("[BOSS] Necron: I understand your words now, my master.")) {
     		stopMusic();
     		checkingon = true;
